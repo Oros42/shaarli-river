5c9a984c138129082a68ddde43fbe94a60348168
Oros42 <oros@ecirtam.net>
2013-12-18 19:53:50 +0100

    Add configurable css style

--- includes/header.php
+++ includes/header.php
@@ -8,7 +8,11 @@
 <link rel="alternate" type="application/rss+xml" href="<?php echo $header_rss; ?>" title="RSS Feed" />
 <?php endif; ?>
 <link href="./assets/bootstrap.min.css" rel="stylesheet">
-<link href="./assets/style.css" rel="stylesheet">
+<?php if(defined('CSS_STYLE') && CSS_STYLE!= ''){
+	echo '<link href="./assets/'.CSS_STYLE.'" rel="stylesheet">';
+}else{
+	echo '<link href="./assets/style.css" rel="stylesheet">';
+} ?>
 <script type="text/javascript" src="./assets/jquery-1.8.3.min.js"></script>
 </head>
 <body>
