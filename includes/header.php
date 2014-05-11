be621a4251d7da911bc2cc4eff4f251a0441e098
Oros42 <oros@ecirtam.net>
2014-05-11 18:26:20 +0200

    Now you can choose your theme.

--- includes/header.php
+++ includes/header.php
@@ -8,11 +8,31 @@
 <link rel="alternate" type="application/rss+xml" href="<?php echo $header_rss; ?>" title="RSS Feed" />
 <?php endif; ?>
 <link href="./assets/bootstrap.min.css" rel="stylesheet">
-<?php if(defined('CSS_STYLE') && CSS_STYLE!= ''){
-	echo '<link href="./assets/'.CSS_STYLE.'" rel="stylesheet">';
-}else{
-	echo '<link href="./assets/style.css" rel="stylesheet">';
-} ?>
+<?php
+$theme_select=null;
+if(!empty($_GET['t']) && (int)$_GET['t'] > 0){
+	$theme_select=(int)$_GET['t'];
+}elseif(!empty($_COOKIE["theme"]) && (int)$_COOKIE["theme"] >0 ){
+	$theme_select=(int)$_COOKIE["theme"];
+}
+if(!empty($theme_select)){
+	$themes = scandir('./themes/');
+	unset($themes[0]);
+	unset($themes[1]);
+	if(isset($themes[$theme_select+1]) && substr($themes[$theme_select+1], -4)=='.css'){
+		$theme='<link href="./themes/'.$themes[$theme_select+1].'" rel="stylesheet">';
+		setcookie("theme", $theme_select);
+	}
+}
+if(empty($theme)){
+	if(defined('CSS_STYLE') && CSS_STYLE!= ''){
+		$theme='<link href="./themes/'.CSS_STYLE.'" rel="stylesheet">';
+	}else{
+		$theme='<link href="./themes/style.css" rel="stylesheet">';
+	}
+}
+echo $theme;
+ ?>
 </head>
 <body>
 <div id="page">
