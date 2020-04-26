5c33b8f4447d7e55f78a0c913ac241406601d21b
Oros42 <oros@ecirtam.net>
2020-04-26 15:22:48 +0200

    update

---
+++ infos.php
@@ -0,0 +1,47 @@
+<?php 
+require_once __DIR__ . '/bootstrap.php';
+include __DIR__ . '/includes/header.php';
+include __DIR__ . '/includes/menu.php';
+?>
+<div class="entry">
+        <h2>Accès via Tor</h2>
+	<a href="https://ecirtamno7a6cynx.onion/shaarlirss/">https://ecirtamno7a6cynx.onion/shaarlirss/</a>
+</div>
+
+<div class="entry">
+	<h2 id="xmpp">Salon de discussions</h2>
+	<ul>
+		<li>Matrix <a href="https://matrix.to/#/#shaarli:feneas.org">https://matrix.to/#/#shaarli:feneas.org</a></li>
+		<li><a href="http://orangina-rouge.org/zbin/?c56d6624235e2ce9#NsQWefj/qUDmaQZe+BM9RwMG0oPnp9hZ5nTYFIzQR5g=">Plus d'infos</a> [old]</li>
+	</ul>
+</div>
+
+<div class="entry">
+	<h2>Contact</h2>
+	<ul>
+		<li>Pour l'ajout ou le retrait d'un shaarli, contactez Oros</li>
+		<li>via shaarlirss[@]ecirtam.net</li>
+		<li>ou par le salon de discussions XMPP (shaarli@conference.leetchee.fr)</li>
+	</ul>
+</div>
+
+<div class="entry">
+	<h2>Projets autour de shaarli</h2>
+	<ul>
+		<li><a href="https://github.com/mknexen/shaarli-river">Shaarli-river (github)</a></li>
+		<li><a href="http://lite.framapad.org/p/hBYMvk4vwG">Shaarli's connection</a></li>
+		<li><a href="http://shaarli.fr/">Shaarlo</a></li>
+		<li><a href="https://github.com/Oros42/find_shaarlis">Où sont les shaarlis ?</a></li>
+	</ul>
+	<h2>Autres Shaarli-river</h2>
+	<ul>
+		<li><a href="https://river.libox.fr/">https://river.libox.fr/</a></li>
+		<li><a href="http://petitetremalfaisant.eu/styx/">http://petitetremalfaisant.eu/styx/</a></li>
+	</ul>
+</div>
+
+<script type="text/javascript">
+document.getElementById('link-infos').className+=' btn-primary';
+</script>
+
+<?php include __DIR__ . '/includes/footer.php'; ?>
